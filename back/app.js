8e1a17c7f877aa6ec322c072d3b18f29b6ba110c
Maor Bar-On <b.maor@growthspace.us>
2021-05-15 23:02:20 +0300

    backend init

---
+++ back/app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (process.env.WHITE_LIST_DOMAINS.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+
+
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+
+
+app.listen(process.env.PORT, () => {
+    console.log( `Started up at port ${process.env.PORT}`);
+});
